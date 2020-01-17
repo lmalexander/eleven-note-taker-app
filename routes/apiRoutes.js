934a4fa4afe362186a173dc66c273c6c41ad311e
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-17 02:08:56 -0600

    wrote post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,46 @@
+// ------------------------- dependencies
+const fs = require("fs");
+
 // ------------------------- load data
-const noteDatabase = require("../journal.json");
+const notePad = require("../notePad.json");
 
 // ------------------------- routing data
 module.exports = function(app) {
     // GET requests 
-    // return notes from journal.json
+    // return notes from notePad.json
     app.get("/api/notes", function(request, response) {
-        response.json(noteDatabase)
+        response.json(notePad);
     });
 
     // POST requests 
-    // add user input response to journal.json
+    // add user input response to notePad.json
     // includes unique ID for note id/deletion
-    // return new note to client 
+    // return notes to client 
     app.post("/api/notes", function(request, response) {
-        const newNote = response.push(request.body);
-        const id = `note#${Math.random()*10}`;
-        journal.json.push(id);
-        response.json(true);
+        // push new note
+        const newNote = request.body;
+        console.log(newNote);
+        notePad.push(newNote);
+        // add id to note
+        notePad.forEach(i => {
+            notePad.id = i + 2020
+        });
+        // return notes
+        response.json(notePad);
     });
 
     // DELETE requests 
     // recieve an ID query of a note to delete
     // return note objects by id for deletion
-    //------- read notes from journal.json
+    //------- read notes from notePad.json
     //------- remove note corresponding to id query
-    //------- rewrite notes to journal.json
+    //------- rewrite notes to notePad.json
     app.delete("/api.notes/:noteID", function(request, response) {
-        if (noteDatabase.id === request.body) {
-            
-        }
+        // read notepad.json file
+        
+        // clear array in the index position corresponding to id query
+        // write notes back to notePad.json
+        // return notes to client
     })
 
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,10 +6,10 @@ module.exports = function (app) {
 // html get requests
 // index.html
 app.get("*", function(response, request) {
-    response.sendFile(path.join(__dirname, "../index.html"));
+    response.sendFile(path.join(__dirname, "../public/.index.html"));
 })
 // notes.html
 app.get("/notes", function(response,request) {
-    response.sendFile(path.join(__dirname, "../notes.html"));
+    response.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 };
