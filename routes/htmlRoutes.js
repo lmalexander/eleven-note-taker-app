de96ec548ec301a60ca191fa757f759e8d951374
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-17 18:46:26 -0600

    fixing html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
 // html get requests
 // index.html
 app.get("/", function(response, request) {
-    response.sendFile(path.join(__dirname, "../index.html"));
+    response.sendFile(path.join(__dirname, "../public/index.html"));
 })
 // notes.html
 app.get("/notes", function(response,request) {
