524ab6d8bc442212573a54c108918f325fd620f5
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-18 22:02:50 -0600

    html routes: removed _dirname, cleaned up comments and format

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,10 +7,10 @@ module.exports = function(app) {
 // ------------------------- html GET request
 // index.html
 app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname, "../public/index.html"));
+    response.sendFile(path.join("../public/index.html"));
 });
 // notes.html
 app.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname, "../public/notes.html"));
+    response.sendFile(path.join("../public/notes.html"));
 });
 };
