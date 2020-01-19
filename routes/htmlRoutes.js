332edbf5cb67dbd066ccabb7689eff3cd53708a1
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-18 23:10:50 -0600

    html routes: changed back to path.join, played with file paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,11 +6,12 @@ module.exports = function(app) {
 // --------------------------------------------- routing
 // ------------------------- html GET request
 // index.html
-app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname, "/index.html"));
-});
+    app.get("/", function(request, response) {
+        response.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
 // notes.html
-app.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname, "/notes.html"));
-});
+    app.get("/notes", function(request, response) {
+        response.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 };
