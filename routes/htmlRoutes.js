aa12f1bef86dbeee1a3c5af10ddea4d9d82a6f08
lmalexander <elizabeth.alexander@northwestern.edu>
2020-01-18 22:14:16 -0600

    html routes: fixed server.js issues (require path, express.static page data parsing) and added _dirname back to htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 // ------------------------- html GET request
 // index.html
 app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname, "/public/index.html"));
+    response.sendFile(path.join(__dirname, "./public/index.html"));
 });
 // notes.html
 app.get("/notes", function(request, response) {
